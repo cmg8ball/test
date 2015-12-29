9ecc6f50d559236b4760f3817ca61b2775b293e8
Colin Goodman <cgoodman8ball@yahoo.co.uk>
2015-12-29 13:02:19 +0000

    Created index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Github Test</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        hi there
+    </body>
+</html>
